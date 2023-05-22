2cbce9a3a7130b409a17e5a979548518f1a4f756
soran <soran.taak@gmail.com>
2023-05-22 20:32:50 +0330

    add scroll to SideSpaceList

--- src/components/sidebar/SideSpaceList.tsx
+++ src/components/sidebar/SideSpaceList.tsx
@@ -23,6 +23,22 @@ export const SideSpaceList = () => {
   };
   return (
     <>
+    <Box
+         height="calc(100vh - 420px )"
+         overflowY="auto"
+         css={{
+          "&::-webkit-scrollbar": {
+           width: "7px",
+          },
+          "&::-webkit-scrollbar-track": {
+           width: "6px",
+          },
+          "&::-webkit-scrollbar-thumb": {
+           background: "#eee",
+           borderRadius: "24px",
+          },
+         }}
+        >
       <Accordion allowToggle>
         <Stack gap="12px">
           {Assigenment.map((item) => (
@@ -93,6 +109,7 @@ export const SideSpaceList = () => {
           ))}
         </Stack>
       </Accordion>
+      </Box>
       <MoreSpaceModal isShow={isOpen} onCloseModal={onCloseModalHandler} />
     </>
   );
