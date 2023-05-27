87c89af84e465826653050986fa0668dd8f0999d
negarin-nb <nnb.se21@gmail.com>
2023-05-27 11:33:39 +0330

    add new task, new column button and filter menu to board

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -6,19 +6,28 @@ import {
   Box,
   Spacer,
   Flex,
-  Divider,
-  Input,
+  Button,
 } from "@chakra-ui/react";
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  useDisclosure,
+} from "@chakra-ui/react";
+
 import ListIcon from "../Icons/listIcon";
 import GridIcon from "../Icons/gridIcon";
 import CalenderIcon from "../Icons/calendarIcon";
 import ShareIcon from "../Icons/shareIcon";
-import { MagnifierIcon } from "../Icons";
-import FilterIcon from "../Icons/filterIcon";
+
 import GridBoardView from "./gridBoardView";
 import ListBoardView from "./listBoardView";
 import MyCalendar from "../MyCalendar";
+import SearchBar from "./searchBar";
+import { ShareProjectModal } from "../Modals/ShareModal/ShareProjectModal";
+import AddIcon from "../Icons/addIcon";
 
 const Board = () => {
   const [tabIndex, setTabIndex] = useState(0);
@@ -26,8 +35,23 @@ const Board = () => {
     setTabIndex(index);
     // console.log(tabIndex);
   };
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const onOpenShareModalHandler = () => {
+    onOpen();
+  };
+  const onCloseShareModalHandler = () => {
+    onClose();
+  };
+
   return (
-    <Box width="75%" height="100vh" py="40px" px="30px">
+    <Box
+      width="calc(100% - 330px)"
+      height="100vh"
+      pt="40px"
+      pr="10px"
+      pl="30px"
+    >
       <Tabs colorScheme="primary" zIndex="2" onChange={handleTabsChange}>
         <TabList border="0px">
           <Flex width="100%" flexDirection="row">
@@ -83,61 +107,27 @@ const Board = () => {
 
             <Spacer />
 
-            <HStack justifyContent="center" alignItems="center">
-              <ShareIcon />
-              <Text fontWeight="500">اشتراک‌گذاری</Text>
+            <HStack
+              onClick={onOpenShareModalHandler}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Button colorScheme="transparent" padding="0px" m="0px">
+                <ShareIcon />
+                <Text mr="5px" color="black" fontWeight="500">
+                  اشتراک‌گذاری
+                </Text>
+              </Button>
             </HStack>
+
+            <ShareProjectModal
+              isShowModal={isOpen}
+              onCloseModal={onCloseShareModalHandler}
+            />
           </Flex>
         </TabList>
 
-        <Divider borderColor="#AAAAAA" zIndex="0" mt="1px" />
-
-        <HStack py="5px">
-          <HStack my="10px" h="25px">
-            <MagnifierIcon />
-            <Input
-              fontSize="12px"
-              width="227px"
-              border="none"
-              type="text"
-              placeholder="جستجو بین تسک‌ها"
-              padding="0"
-              _focusVisible={{ outline: "none", boxShadow: "none" }}
-            />
-
-            <Divider opacity="1" borderColor="#AAAAAA" orientation="vertical" />
-          </HStack>
-
-          <HStack justifyContent="center" alignItems="center" pr="20px">
-            <FilterIcon />
-            <Text fontSize="12px" fontWeight="500">
-              فیلترها
-            </Text>
-          </HStack>
-        </HStack>
-
-        <Divider borderColor="#AAAAAA" />
-
         <TabPanels>
-          <TabPanel
-            css={{
-              "&::-webkit-scrollbar": {
-                height: "6px",
-              },
-              "&::-webkit-scrollbar-track": {
-                width: "7px",
-              },
-              "&::-webkit-scrollbar-thumb": {
-                background: "#eee",
-                borderRadius: "24px",
-              },
-            }}
-            height="80vh"
-            overflowY="auto"
-            px="0"
-          >
-            <ListBoardView />
-          </TabPanel>
           <TabPanel
             css={{
               "&::-webkit-scrollbar": {
@@ -153,16 +143,49 @@ const Board = () => {
             }}
             height="80vh"
             overflowY="auto"
+            p="0"
+          >
+            <SearchBar />
+            <ListBoardView />
+          </TabPanel>
+          <TabPanel
+            css={{
+              "&::-webkit-scrollbar": {
+                width: "6px",
+              },
+              "&::-webkit-scrollbar-track": {
+                width: "7px",
+              },
+              "&::-webkit-scrollbar-thumb": {
+                background: "#eee",
+                borderRadius: "24px",
+              },
+            }}
             pr="0px"
             pl="5px"
+            py="0"
           >
+            <SearchBar />
             <GridBoardView />
           </TabPanel>
-          <TabPanel overflowY="auto" px="0">
+          <TabPanel overflowY="auto" px="0" py="0">
+            <SearchBar />
             {tabIndex === 2 && <MyCalendar />}
           </TabPanel>
         </TabPanels>
       </Tabs>
+      <Button
+        colorScheme="primary"
+        backgroundColor="primary.600"
+        position="fixed"
+        left="50px"
+        bottom="30px"
+        leftIcon={<AddIcon />}
+        zIndex="50"
+        fontSize="14px"
+      >
+        تسک جدید
+      </Button>
     </Box>
   );
 };
