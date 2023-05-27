622efb9f73679c239bfabc5eac2f83f3fe9c5df6
negarin-nb <nnb.se21@gmail.com>
2023-05-27 11:38:01 +0330

    fix measures

--- src/components/MyCalendar/index.tsx
+++ src/components/MyCalendar/index.tsx
@@ -7,35 +7,30 @@ import interactionPlugin from "@fullcalendar/interaction";
 const events = [{ title: "Meeting", start: new Date() }];
 
 export const MyCalendar = () => {
-	return (
-		<Box
-			height="calc(100vh - 210px)"
-			background="#fff"
-			marginTop="25px"
-			paddingBottom="10px"
-		>
-			<FullCalendar
-				headerToolbar={false}
-				direction="rtl"
-				aspectRatio={12}
-				// contentHeight="auto"
-				expandRows={false}
-				plugins={[dayGridPlugin, interactionPlugin]}
-				initialView="dayGridMonth"
-				weekends={true}
-				events={events}
-				// eventContent={renderEventContent}
-				height={"100%"}
-				locale={faLocale}
-				selectable={true}
-				editable={true}
-				dateClick={(e) => alert(Date.parse(e.dateStr))}
-				eventClick={(e) => {
-					console.log(e);
-				}}
-			/>
-		</Box>
-	);
+  return (
+    <Box height="calc(100vh - 210px)" background="#fff">
+      <FullCalendar
+        headerToolbar={false}
+        direction="rtl"
+        aspectRatio={12}
+        // contentHeight="auto"
+        expandRows={false}
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        weekends={true}
+        events={events}
+        // eventContent={renderEventContent}
+        height={"100%"}
+        locale={faLocale}
+        selectable={true}
+        editable={true}
+        dateClick={(e) => alert(Date.parse(e.dateStr))}
+        eventClick={(e) => {
+          console.log(e);
+        }}
+      />
+    </Box>
+  );
 };
 // function renderEventContent(events) {
 // 	return (
