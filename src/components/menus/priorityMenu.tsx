315e80f7f4dfa3f1daa13286ad21a05b2ff8fe7d
negarin-nb <nnb.se21@gmail.com>
2023-05-29 17:51:25 +0330

    add priority menu

---
+++ src/components/menus/priorityMenu.tsx
@@ -0,0 +1,30 @@
+import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+import { FlagIcon } from "../Icons";
+import DeleteIcon from "../Icons/deleteIcon";
+import PriorityList from "../../data/priorityList";
+
+interface PriorityMenuProps {}
+
+const PriorityMenu = () => {
+  return (
+    <Menu>
+      <MenuButton width="145px">اولویت</MenuButton>
+      <MenuList minWidth="145px">
+        {PriorityList.map((data) => (
+          <MenuItem bgColor="white" _hover={{ bgColor: "white" }}>
+            <FlagIcon color={data.color} />
+            <Text pr="5px">{data.name}</Text>
+          </MenuItem>
+        ))}
+
+        <MenuItem bgColor="white" _hover={{ bgColor: "white" }}>
+          <DeleteIcon />
+          <Text pr="5px">حذف اولویت</Text>
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default PriorityMenu;
