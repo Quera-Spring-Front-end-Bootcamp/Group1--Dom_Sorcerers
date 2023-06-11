a8cc06fb53058dac8a0adb8fa0f87ee9ccd4f06c
negarin-nb <nnb.se21@gmail.com>
2023-06-11 15:26:03 +0330

    fix home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,18 @@
-import { useEffect, useState } from "react";
-import authApi from "../../api/auth";
+import { getJwt } from "../../api/auth";
 import Login from "../Auth/login/login";
 import MainPage from "../mainPage/mainPage";
 import { useAuth } from "../../context/authContext";
 
 const Home = () => {
   const authCtx = useAuth();
-  console.log(authCtx.token);
 
+  if (!authCtx.token) {
+    const token = getJwt();
+    if (token) {
+      authCtx.setUserToken(JSON.parse(token));
+    }
+  }
+  console.log(authCtx.token);
   return <>{!!authCtx.token ? <MainPage /> : <Login />}</>;
 };
 
