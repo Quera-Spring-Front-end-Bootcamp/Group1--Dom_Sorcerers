a5725b1d43fc35e86a7fe3103c67cda7eea9b1d4
negarin-nb <nnb.se21@gmail.com>
2023-06-13 18:48:32 +0330

    fix username

--- src/components/sidebar/sidebarUserProfile.tsx
+++ src/components/sidebar/sidebarUserProfile.tsx
@@ -1,8 +1,17 @@
 import { Flex, HStack, Text, Link } from "@chakra-ui/react";
 import { useAuth } from "../../context/authContext";
+import { useState, useEffect } from "react";
 
 const SidebarUserProfile = () => {
   const authCtx = useAuth();
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    if (!authCtx.userData) {
+      const user = localStorage.getItem("userData");
+      if (user) setUsername(JSON.parse(user).username);
+    } else setUsername(authCtx.userData?.username);
+  }, []);
   return (
     <HStack gap="8px">
       <Flex
@@ -17,8 +26,7 @@ const SidebarUserProfile = () => {
       </Flex>
       <Link href="profile">
         <Text fontSize="16px" color="#1E1E1E" fontWeight="500">
-          {authCtx.userData?.username ||
-            JSON.parse(localStorage.getItem("userData") || "").username}
+          {username}
         </Text>
       </Link>
     </HStack>
